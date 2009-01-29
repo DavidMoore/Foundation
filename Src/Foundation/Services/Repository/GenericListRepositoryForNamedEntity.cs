817d2a5d2f2eb827bcc5eb2cd34c480c6c22c195
David.Moore <David.Moore@a2ee4581-b755-4e4a-bf41-49c68440ccba>
2009-01-29 02:50:04 +0000

    git-svn-id: http://dev.digitalgenus.com/svn/david/Foundation/trunk@48 a2ee4581-b755-4e4a-bf41-49c68440ccba

---
+++ Src/Foundation/Services/Repository/GenericListRepositoryForNamedEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Foundation.Services.Security;
+
+namespace Foundation.Services.Repository
+{
+    public class GenericListRepositoryForNamedEntity<T> : GenericListRepository<T>, IEntityWithUniqueNameRepository<T> where T : class, IEntityWithUniqueName, new()
+    {
+        public T Find(string name)
+        {
+            return list.FirstOrDefault(t => t.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}

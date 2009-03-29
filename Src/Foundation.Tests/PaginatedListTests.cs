2a4104a2473592dd660a6c61f84ae6a5989d568c
David.Moore <David.Moore@a2ee4581-b755-4e4a-bf41-49c68440ccba>
2009-03-29 06:20:50 +0000

    git-svn-id: http://dev.digitalgenus.com/svn/david/Foundation/trunk@69 a2ee4581-b755-4e4a-bf41-49c68440ccba

---
+++ Src/Foundation.Tests/PaginatedListTests.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+
+namespace Foundation.Tests
+{
+    [TestFixture]
+    public class PaginatedListTests
+    {
+        [Test]
+        public void Defaults()
+        {
+            var list = new PaginatedList<string>();
+            Assert.IsNotNull(list);
+            Assert.AreEqual(list.Page, 1);
+            Assert.IsFalse(list.HasNextPage);
+            Assert.IsFalse(list.HasPreviousPage);
+            Assert.AreEqual(list.PageCount, 1);
+            Assert.AreEqual(0, list.Count);
+        }
+
+        [Test]
+        public void Setting_up_pagination_with_1_page_of_records_and_setting_total_count()
+        {
+            var results = new[] {"first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth"};
+
+            var list = new PaginatedList<string>(results, 1, 5, 10);
+
+            Assert.AreEqual(10, list.RecordCount);
+            Assert.AreEqual(2, list.PageCount);
+            Assert.AreEqual(1, list.Page);
+        }
+    }
+}
---
+++ Src/Foundation/PaginatedList.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Foundation.Tests
+{
+    public class PaginatedList<T> : List<T>
+    {
+        const int defaultPageSize = 25;
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int RecordCount { get; set; }
+        public int PageCount { get; set; }
+
+        public PaginatedList()
+        {
+            Page = 1;
+            PageSize = defaultPageSize;
+            RecordCount = 0;
+            PageCount = 1;
+        }
+
+        public PaginatedList(IEnumerable<T> source, int page, int pageSize) : this(source, page, pageSize, source.Count()){}
+
+        public PaginatedList(IEnumerable<T> source, int page, int pageSize, int recordCount)
+        {
+            Page = page;
+            PageSize = pageSize;
+            RecordCount = recordCount;
+            PageCount = (int)Math.Ceiling(RecordCount / (double)PageSize);
+
+            AddRange(source);
+        }
+
+        public bool HasPreviousPage { get { return Page > 1; } }
+
+        public bool HasNextPage { get { return (Page < PageCount); } }
+    }
+}

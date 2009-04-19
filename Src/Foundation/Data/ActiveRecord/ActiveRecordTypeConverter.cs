afbd99e6fc4981eea2b0e4cb65a66238729b4989
David.Moore <David.Moore@a2ee4581-b755-4e4a-bf41-49c68440ccba>
2009-04-19 20:06:20 +0000

    git-svn-id: http://dev.digitalgenus.com/svn/david/Foundation/trunk@81 a2ee4581-b755-4e4a-bf41-49c68440ccba

---
+++ Src/Foundation/Data/ActiveRecord/ActiveRecordTypeConverter.cs
@@ -0,0 +1,26 @@
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using Castle.ActiveRecord;
+
+namespace Foundation.Data.ActiveRecord
+{
+    /// <summary>
+    /// Assists model binding with ActiveRecord models, by binding the posted primary key values to the ActiveRecord entity
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ActiveRecordTypeConverter<T> : TypeConverter where T : class
+    {
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            var type = value.GetType();
+
+            return type == typeof(string) ? ActiveRecordMediator<T>.FindByPrimaryKey( int.Parse( (string)value) ) : base.ConvertFrom(context, culture, value);
+        }
+
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
+        }
+    }
+}

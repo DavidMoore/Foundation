5556806d46a7e9456d24c8ccd5cd28888662cb68
David.Moore <David.Moore@a2ee4581-b755-4e4a-bf41-49c68440ccba>
2009-05-20 10:08:03 +0000

    * File rename functionality and tests
    git-svn-id: http://dev.digitalgenus.com/svn/david/Foundation/trunk@90 a2ee4581-b755-4e4a-bf41-49c68440ccba

---
+++ Src/Foundation.Tests/Extensions/FileSystemInfoExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using Foundation.Extensions;
+using NUnit.Framework;
+
+namespace Foundation.Tests.Extensions
+{
+    [TestFixture]
+    public class FileSystemInfoExtensionsTests
+    {
+        [Test]
+        public void Rename()
+        {
+            using( var dir = new TempDirectory() )
+            {
+                var file = dir.CreateTempFile("before_rename.tmp");
+
+                Assert.AreEqual(1, dir.DirectoryInfo.GetFiles("before_rename.tmp").Length);
+                file.Rename("after_rename.tmp");
+                Assert.AreEqual(0, dir.DirectoryInfo.GetFiles("before_rename.tmp").Length);
+                Assert.AreEqual(1, dir.DirectoryInfo.GetFiles("after_rename.tmp").Length);
+            }
+        }
+    }
+}
---
+++ Src/Foundation/Extensions/FileSystemInfoExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Foundation.Extensions
+{
+    public static class FileSystemInfoExtensions
+    {
+        public static void Rename(this FileSystemInfo info, string newName)
+        {
+            var file = info as FileInfo;
+            var dir = info as DirectoryInfo;
+
+            if(file == null && dir == null) throw new ArgumentOutOfRangeException("info", "The passed FileSystemInfo is not a FileInfo or DirectoryInfo");
+
+            if( file != null)
+            {
+                file.MoveTo( Path.Combine(file.DirectoryName, newName) );
+                return;
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}

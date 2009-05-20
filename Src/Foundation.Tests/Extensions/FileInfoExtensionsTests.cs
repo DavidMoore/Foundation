eaa08452c5701386fb3265b3444d53ef21f54e0e
David.Moore <David.Moore@a2ee4581-b755-4e4a-bf41-49c68440ccba>
2009-05-20 09:05:23 +0000

    * Extensions for FileInfo for various aspects of the file name e.g. extension without dot, name without extension
    git-svn-id: http://dev.digitalgenus.com/svn/david/Foundation/trunk@89 a2ee4581-b755-4e4a-bf41-49c68440ccba

---
+++ Src/Foundation.Tests/Extensions/FileInfoExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Foundation.Extensions;
+using NUnit.Framework;
+
+namespace Foundation.Tests.Extensions
+{
+    [TestFixture]
+    public class FileInfoExtensionsTests
+    {
+        [Test]
+        public void NameWithoutExtension()
+        {
+            Assert.AreEqual("test", new FileInfo(@"C:\test.txt").NameWithoutExtension());
+            Assert.AreEqual("test.ing", new FileInfo(@"C:\test.ing.txt").NameWithoutExtension());
+            Assert.AreEqual("test", new FileInfo(@"C:\test").NameWithoutExtension());
+        }
+
+        [Test]
+        public void ExtensionWithNoDot()
+        {
+            Assert.AreEqual("txt", new FileInfo(@"C:\test.txt").ExtensionWithoutDot());
+            Assert.AreEqual("txt", new FileInfo(@"C:\test.ing.txt").ExtensionWithoutDot());
+            Assert.AreEqual("", new FileInfo(@"C:\test").ExtensionWithoutDot());
+        }
+    }
+}
---
+++ Src/Foundation/Extensions/FileInfoExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Foundation.Extensions
+{
+    public static class FileInfoExtensions
+    {
+        /// <summary>
+        /// Gets the name of the file minus the file extension
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public static string NameWithoutExtension(this FileInfo file)
+        {
+            var extensionIndex = file.Name.LastIndexOf(".", StringComparison.OrdinalIgnoreCase);
+
+            if( extensionIndex < 0) return file.Name;
+
+            return file.Name.Substring(0, extensionIndex);
+        }
+
+        /// <summary>
+        /// Returns the extension with the leading dot / period
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public static string ExtensionWithoutDot(this FileInfo file)
+        {
+            return file.Extension.IsNullOrEmpty() ? file.Extension : file.Extension.TrimStart(new[] {'.'});
+        }
+    }
+}

d721d7b546e3d35492b20e198292a2aeb78b46f8
David.Moore <David.Moore@a2ee4581-b755-4e4a-bf41-49c68440ccba>
2009-06-01 03:09:11 +0000

    * Extension method to object for doing a shallow copy clone of an object
    git-svn-id: http://dev.digitalgenus.com/svn/david/Foundation/trunk@92 a2ee4581-b755-4e4a-bf41-49c68440ccba

---
+++ Src/Foundation/Extensions/ObjectExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace Foundation.Extensions
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Does a shallow copy of the object by invoking MemberwiseClone
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static object CloneShallow(this object value)
+        {
+            return value.GetType()
+                .GetMethod("MemberwiseClone", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                .Invoke(value, new object[0]);
+        }
+
+        /// <summary>
+        /// Does a shallow copy of the object by invoking MemberwiseClone
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static T CloneShallow<T>(this object value) where T : class, new()
+        {
+            return (T)value.GetType()
+                .GetMethod("MemberwiseClone", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                .Invoke(value, new object[0]);
+        }
+    }
+}

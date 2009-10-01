21fb7904edd2ffbf6519989362e231594e364391
David.Moore <David.Moore@a2ee4581-b755-4e4a-bf41-49c68440ccba>
2009-10-01 23:02:13 +0000

    * ISynchronizeInvoke extension method for thread-safe invoking
    git-svn-id: http://dev.digitalgenus.com/svn/david/Foundation/trunk@101 a2ee4581-b755-4e4a-bf41-49c68440ccba

---
+++ Src/Foundation/Extensions/ISynchronizeInvokeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using NUnit.Framework;
+
+namespace Foundation.Extensions
+{
+    [TestFixture]
+    public static class ISynchronizeInvokeExtensions
+    {
+        [Test]
+        public static void InvokeEx<T>(this T obj, Action<T> action) where T : ISynchronizeInvoke
+        {
+            if (obj.InvokeRequired)
+            {
+                obj.Invoke(action, new object[] { obj });
+            }
+            else
+            {
+                action(obj);
+            }
+        }
+    }
+}

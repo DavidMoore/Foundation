16d97c77ec9c0623d74410530ef891b214b3e894
David Moore <david@davidmoore.info>
2010-08-28 19:58:20 +1200

    New project for ActiveRecord support: Foundation.Data.ActiveRecord

---
+++ Code/Foundation.Data.ActiveRecord/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Foundation.Data.ActiveRecord")]
+[assembly: AssemblyDescription("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d720a9ba-463a-49a1-bc86-add90b31ae0b")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

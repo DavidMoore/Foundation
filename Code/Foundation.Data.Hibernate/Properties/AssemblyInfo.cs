efc3ebe35ca2699feaec03f05841122ae335cef7
David Moore <david@davidmoore.info>
2010-08-28 19:40:38 +1200

    New library for NHibernate, to de-couple the main library from NHibernate dependency.

---
+++ Code/Foundation.Data.Hibernate/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Foundation.Data.Hibernate")]
+[assembly: AssemblyDescription("NHibernate ORM support.")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c29360d1-88e3-4e3a-9fe6-83f5dbe0ca9c")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

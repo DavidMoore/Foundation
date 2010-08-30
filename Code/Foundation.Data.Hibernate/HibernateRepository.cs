215a33f9d5251d67917b4b25b76c75db85b7afad
David Moore <david@davidmoore.info>
2010-08-30 19:31:29 +1200

    Moved HibernateRepository from tests to Foundation.Data.Hibernate

---
+++ Code/Foundation.Data.Hibernate/HibernateRepository.cs
@@ -0,0 +1,75 @@
+using System.Linq;
+using Foundation.Services.Repository;
+using NHibernate;
+using NHibernate.Linq;
+
+namespace Foundation.Data.Hibernate
+{
+    /// <summary>
+    /// Repository for NHibernate.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class HibernateRepository<T> : IRepository<T> where T : class, new()
+    {
+        readonly ISessionFactory factory;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HibernateRepository&lt;T&gt;"/> class.
+        /// </summary>
+        /// <param name="factory">The session factory.</param>
+        public HibernateRepository(ISessionFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        /// <summary>
+        /// Creates and returns a new instance of the model.
+        /// </summary>
+        /// <returns>A new instance.</returns>
+        public T Create()
+        {
+            return new T();
+        }
+
+        /// <summary>
+        /// Saves the specified instance.
+        /// </summary>
+        /// <param name="instance">The instance.</param>
+        /// <returns></returns>
+        public T Save(T instance)
+        {
+            CurrentSession.SaveOrUpdate(instance);
+            return instance;
+        }
+
+        /// <summary>
+        /// Deletes the instance from the database.
+        /// </summary>
+        /// <param name="instance">The object to remove from the database.</param>
+        public void Delete(T instance)
+        {
+            CurrentSession.Delete(instance);
+        }
+
+        /// <summary>
+        /// Returns a queryable list of all the instances of the model.
+        /// </summary>
+        /// <returns></returns>
+        public IQueryable<T> Query()
+        {
+            return CurrentSession.Linq<T>();
+        }
+
+        /// <summary>
+        /// Gets the current session.
+        /// </summary>
+        /// <value>The current session.</value>
+        internal ISession CurrentSession
+        {
+            get
+            {
+                return factory.GetCurrentSession();
+            }
+        }
+    }
+}
--- Tests/UnitTests/Foundation.Tests/Data/NHibernate/HibernateRepositoryTests.cs
+++ Tests/UnitTests/Foundation.Tests/Data/NHibernate/HibernateRepositoryTests.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Linq;
-using Foundation.Services.Repository;
+﻿using Foundation.Data.Hibernate;
 using Foundation.Tests.Data.Hierarchy;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using NHibernate;
-using NHibernate.Linq;
 
 namespace Foundation.Tests.Data.NHibernate
 {
@@ -61,85 +58,8 @@ namespace Foundation.Tests.Data.NHibernate
         [TestMethod]
         public void List()
         {
-            repository.Query();
             // TODO: How to verify this?
-        }
-    }
-
-    /// <summary>
-    /// Repository for NHibernate.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class HibernateRepository<T> : IRepository<T> where T : class, new()
-    {
-        readonly ISessionFactory factory;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="HibernateRepository&lt;T&gt;"/> class.
-        /// </summary>
-        /// <param name="factory">The session factory.</param>
-        public HibernateRepository(ISessionFactory factory)
-        {
-            this.factory = factory;
-        }
-
-        /// <summary>
-        /// Creates and returns a new instance of the model.
-        /// </summary>
-        /// <returns>A new instance.</returns>
-        public T Create()
-        {
-            return new T();
-        }
-
-        /// <summary>
-        /// Saves the specified instance.
-        /// </summary>
-        /// <param name="instance">The instance.</param>
-        /// <returns></returns>
-        public T Save(T instance)
-        {
-            CurrentSession.SaveOrUpdate(instance);
-            return instance;
-        }
-
-        /// <summary>
-        /// Deletes all instances from the database. You can modify this
-        /// query to limit the instances deleted (for example, just a single instance).
-        /// </summary>
-        public IQueryable<T> Delete()
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Deletes the instance from the database.
-        /// </summary>
-        /// <param name="instance">The object to remove from the database.</param>
-        public void Delete(T instance)
-        {
-            CurrentSession.Delete(instance);
-        }
-
-        /// <summary>
-        /// Returns a queryable list of all the instances of the model.
-        /// </summary>
-        /// <returns></returns>
-        public IQueryable<T> Query()
-        {
-            return CurrentSession.Linq<T>();
-        }
-
-        /// <summary>
-        /// Gets the current session.
-        /// </summary>
-        /// <value>The current session.</value>
-        internal ISession CurrentSession
-        {
-            get
-            {
-                return factory.GetCurrentSession();
-            }
+            repository.Query();
         }
     }
 }

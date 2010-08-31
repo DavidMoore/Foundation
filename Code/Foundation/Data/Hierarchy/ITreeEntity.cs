58b450d11b82dbac5d176cec2a6b6e3dccbbb937
David Moore <david@davidmoore.info>
2010-08-31 12:01:19 +1200

    Documentation

--- Code/Foundation/Data/Hierarchy/ITreeEntity.cs
+++ Code/Foundation/Data/Hierarchy/ITreeEntity.cs
@@ -3,12 +3,18 @@ using Foundation.Models;
 namespace Foundation.Data.Hierarchy
 {
     /// <summary>
-    /// Marks an entity as having tree hierarchy info
+    /// Marks an entity as having a place in a tree hierarchy.
+    /// As an example, a Category entity may have a Parent Category and multiple
+    /// child Categories.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <typeparam name="TId">The type of the identifier field <see cref="IEntity{T}.Id"/></typeparam>
+    /// <typeparam name="T">The type of the entity.</typeparam>
+    /// <typeparam name="TId">The type of the identifier field of the entity.</typeparam>
     public interface ITreeEntity<T,TId> where T : class, ITreeEntity<T, TId>, IEntity<TId>
     {
+        /// <summary>
+        /// Gets an object that contains information about the entity's place in the tree.
+        /// </summary>
+        /// <value>Information about the entity's place in the tree.</value>
         TreeInfo<T,TId> Tree { get; }
     }
 }

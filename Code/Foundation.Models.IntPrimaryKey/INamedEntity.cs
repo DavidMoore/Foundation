ac6cbfc2fa9b695752bc3dbdc84340c709a30951
David Moore <david@davidmoore.info>
2010-10-30 19:50:02 +1300

    INamedEntity interface for Int32 primary keys altered to explicitly implement IEntity

--- Code/Foundation.Models.IntPrimaryKey/INamedEntity.cs
+++ Code/Foundation.Models.IntPrimaryKey/INamedEntity.cs
@@ -3,5 +3,5 @@
     /// <summary>
     /// An entity that possesses a unique <see cref="IEntity{T}.Id"/> and unique <see cref="INamedEntity{T}.Name"/>.
     /// </summary>
-    public interface INamedEntity : INamedEntity<int> {}
+    public interface INamedEntity : INamedEntity<int>, IEntity {}
 }

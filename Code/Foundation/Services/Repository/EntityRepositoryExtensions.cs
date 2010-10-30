ce276dc0b50c98ea33d1b3c2d93a8330f90920b7
David Moore <david@davidmoore.info>
2010-10-30 19:49:18 +1300

    SingleOrDefault extension method for Entity repositories

---
+++ Code/Foundation/Services/Repository/EntityRepositoryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Foundation.Models;
+
+namespace Foundation.Services.Repository
+{
+    public static class EntityRepositoryExtensions
+    {
+        /// <summary>
+        /// Gets the entity with the specified primary key, or <c>null</c> if it can't be found.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TPrimaryKey">The type of the primary key.</typeparam>
+        /// <param name="repository">The repository containing the entities.</param>
+        /// <param name="key">The value of the primary key.</param>
+        /// <returns>The entity identified by the primary key of <paramref name="key"/>, otherwise <c>null</c> if one wasn't found.</returns>
+        public static T SingleOrDefault<T, TPrimaryKey>(this IRepository<T> repository, TPrimaryKey key) where T : class, IEntity<TPrimaryKey>
+        {
+            return repository.Query().SingleOrDefault(entity => entity.Id.Equals(key));
+        }
+    }
+}

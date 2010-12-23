c742aa692724c958fc5f3f43da2ef0baf3d8104d
David Moore <david@davidmoore.info>
2010-12-23 23:02:56 +1300

    ReadChunks extension method for Stream. Enumerates the file contents as chunks of data (byte buffers) at a specified chunk size, until the end of the file is reached.

---
+++ Code/Foundation/ExtensionMethods/StreamExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Foundation.ExtensionMethods
+{
+    public static class StreamExtensions
+    {
+        /// <summary>
+        /// Reads in the contents of the stream in chunks, yielding
+        /// each chunk per iteration until the end of the stream is reached.
+        /// </summary>
+        /// <param name="stream">The stream to read.</param>
+        /// <param name="chunkSize">Size of the chunks to read.</param>
+        /// <returns></returns>
+        public static IEnumerable<byte[]> ReadChunks(this Stream stream, int chunkSize)
+        {
+            var buffer = new byte[chunkSize];
+            int bytesRead;
+            while( (bytesRead = stream.Read(buffer, 0, chunkSize)) > 0 )
+            {
+                if (bytesRead == chunkSize)
+                {
+                    yield return buffer;
+                }
+                else
+                {
+                    var smallerBuffer = new byte[bytesRead];
+                    Array.Copy(buffer,smallerBuffer,bytesRead);
+                    yield return smallerBuffer;
+                }
+            }
+        }
+    }
+}
---
+++ Tests/UnitTests/Foundation.Tests/ExtensionMethods/StreamExtensionsTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using Foundation.ExtensionMethods;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Foundation.Tests.ExtensionMethods
+{
+    [TestClass]
+    public class StreamExtensionsTests
+    {
+        [TestMethod]
+        public void ReadChunks()
+        {
+            const int chunkSize = 1024;
+
+            var random = new Random();
+
+            // Use a random amount of chunks; 5 to 15
+            var chunks = random.Next(5, 15);
+
+            using (var temp = new TempFile())
+            {
+                // Write the chunks to the file
+                using (var stream = temp.FileInfo.OpenWrite())
+                {
+                    for (var i = chunks; i > 0; i--)
+                    {
+                        var buffer = Enumerable.Repeat<byte>(255, chunkSize).ToArray();
+                        stream.Write(buffer, 0, chunkSize);
+                    }
+                }
+
+                temp.FileInfo.Refresh();
+
+                var fileLength = temp.FileInfo.Length;
+
+                // Now read in the chunks
+                var chunkCount = 0;
+                var totalBytes = 0;
+
+                foreach (var chunk in temp.FileInfo.OpenRead().ReadChunks(chunkSize))
+                {
+                    Assert.IsTrue(  chunk.All(b => b == 255) );
+                    chunkCount++;
+                    totalBytes += chunk.Length;
+                }
+
+                Assert.AreEqual(chunks, chunkCount);
+                Assert.AreEqual(fileLength, totalBytes);
+            }
+        }
+
+        
+    }
+}

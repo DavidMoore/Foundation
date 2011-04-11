7107753eda4b5865b8ee3f89906e33ebfdb19409
David Moore <david@davidmoore.info>
2011-04-11 21:26:36 +1200

    String result handler for file search results

---
+++ Code/Foundation/Windows/IO/StringResultHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Security;
+
+namespace Foundation.Windows.IO
+{
+    public class StringResultHandler : SearchResultHandler<string>
+    {
+        readonly bool includeDirectories;
+        readonly bool includeFiles;
+
+        public StringResultHandler(bool includeFiles, bool includeDirectories)
+        {
+            this.includeFiles = includeFiles;
+            this.includeDirectories = includeDirectories;
+        }
+
+        [SecurityCritical]
+        internal override bool IsResultIncluded(SearchResult result)
+        {
+            var includeFile = includeFiles && result.FindData.IsFile;
+            var includeDir = includeDirectories && result.FindData.IsDir;
+            Contract.Assert(!(includeFile && includeDir), result.FindData.FileName + ": current item can't be both file and dir!");
+            return (includeFile || includeDir);
+        }
+
+        [SecurityCritical]
+        internal override String CreateObject(SearchResult result)
+        {
+            return result.UserPath;
+        }
+    }
+}

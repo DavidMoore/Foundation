254c30726f4ec73c7b4b389ac898b243d683d503
David Moore <david@davidmoore.info>
2011-04-11 20:26:52 +1200

    Find File API and model tests

---
+++ Tests/UnitTests/Foundation.Tests/Windows/IO/FindFileApiTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Foundation.Windows;
+using Foundation.Windows.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Foundation.Tests.Windows.IO
+{
+    [TestClass]
+    public class FindFileApiTests
+    {
+        [TestMethod]
+        public void FindFirstFile()
+        {
+            using (var tempDir = new TempDirectory())
+            {
+                var tempFile = tempDir.CreateTempFile("firstFile.txt");
+
+                var data = new FindData();
+
+                using (var handle = Win32Api.IO.FindFirstFile(tempFile.FullName, data))
+                {
+                    Assert.IsFalse(handle.IsInvalid);
+                    Assert.IsFalse(handle.IsClosed);
+                }
+            }
+        }
+    }
+}

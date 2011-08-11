8448605189e0d7a6f997631e5bc4d4da0e9a6c5f
David Moore <emaildavidmoore@gmail.com>
2011-08-11 16:13:42 +1200

    Updated the test code for harvesting VB6 references

--- Tests/UnitTests/Foundation.Tests/Build/MSBuild/VisualBasicProjectTests.cs
+++ Tests/UnitTests/Foundation.Tests/Build/MSBuild/VisualBasicProjectTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using Foundation.Build.MSBuild;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -10,16 +7,41 @@ namespace Foundation.Tests.Build.MSBuild
     [TestClass]
     public class VisualBasicProjectTests
     {
+        private const string visualBasicProjectLines = @"Type=Exe
+Reference=*\G{198B1AD5-89DD-4074-8033-7FC78B46C74F}#3.b#0#..\..\..\..\..\SubFolder\ExeReference.exe#ExeReferenceName
+Reference=*\G{198B1AD5-89DD-4074-8033-7FC78B46C74F}#3.b#0#..\..\..\..\..\SubFolder\DllReference.dll#DllReferenceName
+Object={F9043C88-F6F2-101A-A3C9-08002B2F49FB}#1.2#0; ComDlg32.ocx
+Object={831FDD16-0C5C-11D2-A9FC-0000F8754DA1}#2.0#0; MSCOMCTL.OCX
+Form=frmForm.frm
+Module=modModule; modModule.bas";
+
         [TestMethod]
         public void GetFileNameFromReference()
         {
             Assert.AreEqual("stdole2.tlb", VisualBasicProject.GetFileNameFromReference(@"*\G{00020430-0000-0000-C000-000000000046}#2.0#0#..\..\..\..\..\..\..\Windows\SysWOW64\stdole2.tlb#OLE Automation"));
             Assert.AreEqual("dbgwproc.dll", VisualBasicProject.GetFileNameFromReference(@"*\G{3D0758FA-4171-11D0-A747-00A0C91110C3}#a.0#0#dbgwproc.dll#Debug Object for AddressOf Subclassing"));
             Assert.AreEqual("DPDEVICE.DLL", VisualBasicProject.GetFileNameFromReference(@"*\G{AAAE3FBC-3DCA-11D2-B50B-00A024C9FA1C}#1.4#0#..\..\..\WINDOWS\system32\DPDEVICE.DLL#Dictaphone Device Object Library"));
-//Object={F748808D-5300-489C-8DC1-578AA5871056}#1.0#0; USBMicCtrl.dll
-//Object={D8F5B61D-9152-4399-BF30-A1E4F3F072F6}#4.0#0; IGTabs40.ocx
-//Object={648A5603-2C6E-101B-82B6-000000000014}#1.1#0; MSCOMM32.OCX
-//Object={F304A4CF-FB7A-43C4-A860-3F53C550C375}#1.0#0; PSMike.OCX
+            Assert.AreEqual("MSCOMM32.OCX", VisualBasicProject.GetFileNameFromReference(@"Object={648A5603-2C6E-101B-82B6-000000000014}#1.1#0; MSCOMM32.OCX"));
+        }
+
+        [TestMethod]
+        public void HarvestReferenceFilenames()
+        {
+            var filenames = VisualBasicProject.HarvestReferences(visualBasicProjectLines);
+            Assert.IsNotNull(filenames);
+            Assert.AreEqual(2, filenames.Count());
+            Assert.AreEqual("ExeReference.exe", filenames.First());
+            Assert.AreEqual("DllReference.dll", filenames.Skip(1).First());
+        }
+
+        [TestMethod]
+        public void HarvestComponentFilenames()
+        {
+            var filenames = VisualBasicProject.HarvestComponents(visualBasicProjectLines);
+            Assert.IsNotNull(filenames);
+            Assert.AreEqual(2, filenames.Count());
+            Assert.AreEqual("ComDlg32.ocx", filenames.First());
+            Assert.AreEqual("MSCOMCTL.OCX", filenames.Skip(1).First());
         }
     }
 }

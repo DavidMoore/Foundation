ba92c6acac598d26c08e23bf25f1226bdb560081
David Moore <emaildavidmoore@gmail.com>
2011-08-18 18:21:17 +1200

    In ServiceRegistration, refactored the exception messages so that the correct attribute is named, and also checked to make sure contracts have a namespace

--- Code/Foundation/Services/Registration/ServiceRegistration.cs
+++ Code/Foundation/Services/Registration/ServiceRegistration.cs
@@ -29,10 +29,12 @@ namespace Foundation.Services.Registration
                     var interfaces = type.GetInterfaces().ToList();
                     if (interfaces.Count == 0)
                         throw new ServiceRegistrationException(
-                            "The service '{0}' does not have any contract interfaces! Ensure it implements a contract, or manually specify the contract type in the [RegisterService] attribute.",
-                            type);
+                            "The service '{0}' does not have any contract interfaces! Ensure it implements a contract, or manually specify the contract type in the [{1}] attribute.",
+                            type, typeof(RegisterComponentAttribute).Name);
 
                     contractType = interfaces.First();
+
+                    if( contractType.Namespace == null ) throw new ServiceRegistrationException("The contract type {0} for type {1} doesn't have a Namespace!", contractType, type);
 
                     // If it's a dynamically composed UI class, such as a WPF UserControl, the top-most interface
                     // will be from System.Windows.Media.Composition; in which case, we want to try to get the next one up
@@ -40,8 +42,8 @@ namespace Foundation.Services.Registration
                     {
                         if (interfaces.Count < 2)
                             throw new ServiceRegistrationException(
-                                "The service {0} doesn't have a base contract interface! Ensure it implements a contract, or manually specify the contract type in the [RegisterService] attribute.",
-                                type);
+                                "The service {0} doesn't have a base contract interface! Ensure it implements a contract, or manually specify the contract type in the [{1}] attribute.",
+                                type, typeof(RegisterComponentAttribute).Name);
                         contractType = interfaces[interfaces.Count - 2];
                     }
                 }

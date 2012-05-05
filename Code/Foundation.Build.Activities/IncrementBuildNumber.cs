ddfe87880386546034dd9a8550c6fb0a04018439
David Moore <emaildavidmoore@gmail.com>
2012-05-06 00:13:57 +0100

    Initial commit of start of error analyzer app

---
+++ Code/Foundation.Build.Activities/IncrementBuildNumber.cs
@@ -0,0 +1,77 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IncrementBuildNumber.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Activities;
+using System.ComponentModel;
+using System.Drawing;
+
+using Foundation.Build.Activities.Properties;
+
+using Microsoft.TeamFoundation.Build.Client;
+using Microsoft.TeamFoundation.Build.Workflow.Activities;
+using Microsoft.TeamFoundation.Build.Workflow.Design;
+
+namespace Foundation.Build.Activities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    [BuildCategory]
+    [BuildActivity(HostEnvironmentOption.All)]
+    [Designer(typeof(TeamBuildBaseActivityDesigner))]
+    [ToolboxBitmap(typeof(TeamBuildBaseActivityDesigner), "DefaultBuildIcon.png")]
+    public sealed class IncrementBuildNumber : CodeActivity<string>
+    {
+        private readonly IBuildQueryProvider queryProvider;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpdateBuildNumber"/> class.
+        /// </summary>
+        public IncrementBuildNumber() : this(new BuildQueryProvider()) {}
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpdateBuildNumber"/> class.
+        /// </summary>
+        /// <param name="queryProvider">The query provider.</param>
+        public IncrementBuildNumber(IBuildQueryProvider queryProvider)
+        {
+            this.queryProvider = queryProvider;
+        }
+        
+        protected override string Execute(CodeActivityContext context)
+        {
+            var buildDetail = context.GetExtension<IBuildDetail>();
+            int num = 0;
+        Label_0021:
+            //buildDetail.BuildNumber = FormatStringToBuildNumber(stringVar, buildDetail, null, false);
+            try
+            {
+                buildDetail.Save();
+            }
+            catch (BuildNumberAlreadyExistsException)
+            {
+                if (num++ >= 100)
+                {
+                    throw;
+                }
+                goto Label_0021;
+            }
+            return buildDetail.BuildNumber;
+        }
+
+        /// <summary>
+        /// Caches the metadata.
+        /// </summary>
+        /// <param name="metadata">The metadata.</param>
+        protected override void CacheMetadata(CodeActivityMetadata metadata)
+        {
+            base.CacheMetadata(metadata);
+            metadata.RequireExtension<IBuildDetail>();
+        }
+    }
+}
---
+++ Code/Foundation.ErrorAnalyzer/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Foundation.Windows;
+
+namespace Foundation.ErrorAnalyzer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var errorCodeString = errorCode.Text.Trim();
+
+            var result = Win32Error.MakeHRFromErrorCode(int.Parse(errorCodeString));
+
+            var ex = Marshal.GetExceptionForHR(result);
+            textBox2.Text = ex.ToString();
+        }
+    }
+}

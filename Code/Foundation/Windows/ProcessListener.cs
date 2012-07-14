a32442085542b3ab965253b1da7e709b5da150a5
David Moore <emaildavidmoore@gmail.com>
2012-07-14 23:49:52 +0100

    Moved the ProcessListener to the Foundation project

---
+++ Code/Foundation/Windows/ProcessListener.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Management;
+
+namespace Foundation.Windows
+{
+    /// <summary>
+    /// Event management for when any system process (or a process specified by name) is created or exited.
+    /// </summary>
+    public class ProcessListener : IDisposable
+    {
+        readonly string processName;
+        readonly ManagementEventWatcher processStartedWatcher;
+        readonly ManagementEventWatcher processExitedWatcher;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProcessListener"/> class.
+        /// </summary>
+        /// <param name="processName">Name of the process to monitor.</param>
+        public ProcessListener(string processName)
+        {
+            this.processName = processName;
+
+            // Poll every second for new processes.
+            var query = "SELECT TargetInstance  FROM __InstanceCreationEvent WITHIN  1  WHERE TargetInstance ISA \'Win32_Process\'";
+
+            // If specified, monitor for an exact process name.
+            if (!string.IsNullOrEmpty(processName)) query += " AND TargetInstance.Name = '" + processName + "'";
+
+            processStartedWatcher = new ManagementEventWatcher(@"\\.\root\CIMV2", query);
+            processStartedWatcher.EventArrived += OnProcessStarted;
+            processStartedWatcher.Start();
+
+            query = "SELECT TargetInstance  FROM __InstanceDeletionEvent WITHIN  1  WHERE TargetInstance ISA \'Win32_Process\'";
+
+            // If specified, monitor for an exact process name.
+            if (!string.IsNullOrEmpty(processName)) query += " AND TargetInstance.Name = '" + processName + "'";
+
+            processExitedWatcher = new ManagementEventWatcher(@"\\.\root\CIMV2", query);
+            processExitedWatcher.EventArrived += OnProcessExited;
+            processExitedWatcher.Start();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProcessListener"/> class.
+        /// </summary>
+        public ProcessListener() : this(null) {}
+
+        /// <summary>
+        /// Occurs when a process has been started.
+        /// </summary>
+        public event EventHandler<EventArrivedEventArgs> ProcessStarted;
+        
+        /// <summary>
+        /// Occurs when a process has exited.
+        /// </summary>
+        public event EventHandler<EventArrivedEventArgs> ProcessExited;
+
+        private void OnProcessStarted(object sender, EventArrivedEventArgs e)
+        {
+            if (ProcessStarted != null) ProcessStarted(sender, e);
+        }
+
+        private void OnProcessExited(object sender, EventArrivedEventArgs e)
+        {
+            if (ProcessExited != null) ProcessExited(sender, e);
+        }
+        
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        /// <filterpriority>2</filterpriority>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        void Dispose(bool disposing)
+        {
+            if (!disposing) return;
+            if (processStartedWatcher != null) processStartedWatcher.Dispose();
+            if (processExitedWatcher != null) processExitedWatcher.Dispose();
+        }
+    }
+}
--- Tests/UnitTests/Foundation.Tests/Windows/ProcessListenerTests.cs
+++ Tests/UnitTests/Foundation.Tests/Windows/ProcessListenerTests.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Management;
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+
+using Foundation.Windows;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -142,73 +143,6 @@ namespace Foundation.Tests.Windows
                     Assert.AreEqual(true, processWasEnded);
                 }
             }
-        }
-    }
-
-    public class ProcessListener : IDisposable
-    {
-        readonly string processName;
-        readonly ManagementEventWatcher processStartedWatcher;
-        readonly ManagementEventWatcher processExitedWatcher;
-
-        public ProcessListener(string processName)
-        {
-            this.processName = processName;
-
-            // Poll every second for new processes.
-            var query = "SELECT TargetInstance  FROM __InstanceCreationEvent WITHIN  1  WHERE TargetInstance ISA \'Win32_Process\'";
-
-            // If specified, monitor for an exact process name.
-            if (!string.IsNullOrEmpty(processName)) query += " AND TargetInstance.Name = '" + processName + "'";
-
-            processStartedWatcher = new ManagementEventWatcher(@"\\.\root\CIMV2", query);
-            processStartedWatcher.EventArrived += OnProcessStarted;
-            processStartedWatcher.Start();
-
-            query = "SELECT TargetInstance  FROM __InstanceDeletionEvent WITHIN  1  WHERE TargetInstance ISA \'Win32_Process\'";
-
-            // If specified, monitor for an exact process name.
-            if (!string.IsNullOrEmpty(processName)) query += " AND TargetInstance.Name = '" + processName + "'";
-
-            processExitedWatcher = new ManagementEventWatcher(@"\\.\root\CIMV2", query);
-            processExitedWatcher.EventArrived += OnProcessExited;
-            processExitedWatcher.Start();
-        }
-
-        public ProcessListener() : this(null) {}
-
-        public event EventHandler<EventArrivedEventArgs> ProcessStarted;
-        public event EventHandler<EventArrivedEventArgs> ProcessExited;
-
-        private void OnProcessStarted(object sender, EventArrivedEventArgs e)
-        {
-            if (ProcessStarted != null) ProcessStarted(sender, e);
-//            var targetInstance = (ManagementBaseObject)e.NewEvent.Properties["TargetInstance"].Value;
-//            string name = targetInstance.Properties["Name"].Value.ToString();
-//            Console.WriteLine("{0} process started", name);
-//            Debug.WriteLine("{0} process started", name);
-        }
-
-        private void OnProcessExited(object sender, EventArrivedEventArgs e)
-        {
-            if (ProcessExited != null) ProcessExited(sender, e);
-        }
-        
-        /// <summary>
-        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-        /// </summary>
-        /// <filterpriority>2</filterpriority>
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        void Dispose(bool disposing)
-        {
-            if (!disposing) return;
-            if (processStartedWatcher != null) processStartedWatcher.Dispose();
-            if (processExitedWatcher != null) processExitedWatcher.Dispose();
         }
     }
 }

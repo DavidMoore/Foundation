11102e1643799c43cafce9b55d2e5dcd301769df
David Moore <emaildavidmoore@gmail.com>
2012-08-20 17:02:35 +1200

    Made all internals visible to the unit test project

--- Code/Foundation.Data.Hibernate/Properties/AssemblyInfo.cs
+++ Code/Foundation.Data.Hibernate/Properties/AssemblyInfo.cs
@@ -1,5 +1,7 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("NHibernate ORM support.")]
 [assembly: Guid("c29360d1-88e3-4e3a-9fe6-83f5dbe0ca9c")]
+[assembly: InternalsVisibleTo("Foundation.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010087d332e80809acf066d0c4360316e881f059f47cfcf432f4882d64d667dc975a5dea3b9e76a8138917979de05b3c94cae5f32c2f14d90ad9d0da56e32bf5d7bbf1d4952bac16233aa13eaf760bcc4c04a81ebcb02a55211c805462cc9ff5b3388874c033b19d2c1577538ee9f898616d9360d9f0ad16e336074a781dc3bca3bf")]

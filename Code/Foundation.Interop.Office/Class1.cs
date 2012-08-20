b3946421592d61b9f64f5e1a9bdb1e1788ff633d
David Moore <emaildavidmoore@gmail.com>
2012-08-20 17:06:59 +1200

    Added Visual Studio VB6 integration project back into the solution, and added a new project for Office integration

---
+++ Code/Foundation.Interop.Office/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Foundation.Interop.Office
+{
+    public class Class1
+    {
+    }
+}
--- Tests/UnitTests/Foundation.Tests/Interop/Office/DocumentConverterTests.cs
+++ Tests/UnitTests/Foundation.Tests/Interop/Office/DocumentConverterTests.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections;
+using System.IO;
+using System.IO.Packaging;
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Documents;
+
+using DocumentFormat.OpenXml.Packaging;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Moq;
@@ -24,8 +30,35 @@ namespace Foundation.Tests.Interop.Office
         void Convert(IWordDocument document);
     }
 
+    class WordDocumentToXpsConverter : IWordDocumentConverter
+    {
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing) return;
+        }
+
+        public void Convert(IWordDocument doc)
+        {
+            using (var stream = new MemoryStream(4096))
+            using (var document = doc.GetWordprocessingDocument())
+            using (var package = Package.Open(stream, FileMode.CreateNew, FileAccess.ReadWrite))
+            {
+                //document.GetPartsOfType<>()
+                
+            }
+        }
+    }
+
     public interface IWordDocument : IDisposable
     {
         IDictionary<string, string> Bookmarks { get; }
+        
+        WordprocessingDocument GetWordprocessingDocument();
     }
 }

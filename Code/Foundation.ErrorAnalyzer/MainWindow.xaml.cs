117d8ab61a3b4e8eb14d10a7baf50fc684bde205
David Moore <emaildavidmoore@gmail.com>
2012-10-15 17:26:16 +1300

    Initial error analyzer tool

--- Code/Foundation.ErrorAnalyzer/MainWindow.xaml.cs
+++ Code/Foundation.ErrorAnalyzer/MainWindow.xaml.cs
@@ -12,16 +12,21 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 using Foundation.Windows;
 
 namespace Foundation.ErrorAnalyzer
 {
+    using System.Globalization;
+    using System.Text.RegularExpressions;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        const string hexString = "^(0[xX]|&[hH])?(?<number>[0-9a-zA-Z]{8}|[0-9a-zA-Z]{16})$";
+        static readonly Regex hexRegex = new Regex(hexString, RegexOptions.Compiled);
+
         public MainWindow()
         {
             InitializeComponent();
@@ -29,12 +34,39 @@ namespace Foundation.ErrorAnalyzer
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            var errorCodeString = errorCode.Text.Trim();
+            var code = errorCode.Text.Trim();
+            var isHex = hexRegex.IsMatch(code);
 
-            var result = Win32Error.MakeHRFromErrorCode(int.Parse(errorCodeString));
+            int integer;
 
-            var ex = Marshal.GetExceptionForHR(result);
-            textBox2.Text = ex.ToString();
+            if (isHex)
+            {
+                code = hexRegex.Match(code).Groups["number"].Value;
+                integer = int.Parse(code, NumberStyles.HexNumber, CultureInfo.CurrentCulture.NumberFormat);
+            }
+            else
+            {
+                integer = int.Parse(code, CultureInfo.CurrentCulture.NumberFormat);
+            }
+
+            // Convert to a proper HRESULT code, padding it out.
+            // For short error codes, for example error code 2 (ERROR_FILE_NOT_FOUND), they need
+            // to be padded to a full HRESULT length, so 0x00000002 becomes 0x80070002.
+            if ((integer & 2147483648L) != 2147483648L)
+            {
+                Console.WriteLine("Error message: {0}", Win32Error.GetMessage(integer));
+                integer = integer & ushort.MaxValue | -2147024896;
+            }
+
+            Console.WriteLine("Decimal value: {0}", integer);
+            Console.WriteLine("    Hex value: 0x{0:x}", integer);
+
+            // Treat as an hresult
+            var exception = Marshal.GetExceptionForHR(integer);
+
+            Console.WriteLine("Exception: {0}", exception);
+
+            textBox2.Text = exception.ToString();
         }
     }
 }
